70b3f1893c60dce2ffc3ab7b2c070740321b7d51
Sebastin Raj <134362610+sebastin6000@users.noreply.github.com>
2023-07-06 00:18:54 +0530

    implemented post routes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import express from "express";
 // creating the app
 const app = express();
 
+//json middleware for parse the data from the client
+app.use(express.json());
+
 ///listening for request
 const PORT = 3000;
 app.listen(PORT, () => {});
@@ -17,6 +20,16 @@ app.get("api/v1/tours", (request, response) => {
     .json({ status: "sucess", results: tours.length, data: { tours } });
 });
 
-app.post("/", (request, response) => {
-  response.status(200).json({ message: "you can post to this route" });
+app.post("api/v1/tours", (request, response) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, request.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/tours-simple.json`,
+    JSON.stringify(tours),
+    (error) => {
+      response.status(201).json({ status: "success", data: { tour: newTour } });
+    }
+  );
 });
