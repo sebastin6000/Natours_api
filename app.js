e7491e94202c79a978de97850ca5fe26b1b625df
Sebastin Raj <134362610+sebastin6000@users.noreply.github.com>
2023-07-05 23:20:24 +0530

    handled get requests for tours route

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const app = express();
 const PORT = 3000;
 app.listen(PORT, () => {});
 
-// defining simple route
-app.get("/", (request, response) => {
-  response.status(200).json({ message: "hello from the server" });
+// implementing tours route
+const fs = require("fs");
+const tours = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`);
+app.get("api/v1/tours", (request, response) => {
+  // here the json responce is coded in jsend standard format
+  response
+    .status(200)
+    .json({ status: "sucess", results: tours.length, data: { tours } });
 });
 
 app.post("/", (request, response) => {
